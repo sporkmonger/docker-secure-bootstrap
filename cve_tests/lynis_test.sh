7fdec27e36118ee75bc866fef4824ff57cb67d52
Bob Aman <bob@sporkmonger.com>
2015-07-17 21:52:43 -0700

    Fail the build if lynis has non-zero exit code.

--- cve_tests/lynis_test.sh
+++ cve_tests/lynis_test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 EXITCODE=0
 
-cd /opt/bin/lynis && lynis -Q audit system < /dev/null && cd -
+cd /opt/bin/lynis
+/opt/bin/lynis -Q audit system < /dev/null
+if [ $? -ne 0 ]
+then
+  EXITCODE=$((EXITCODE+1))
+fi
+cd -
 
 exit $EXITCODE
